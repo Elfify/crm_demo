4a798fcebca4eb3a4547c6a3e4937c718f5f8ba9
Weston Sandfort <sandfortw@gmail.com>
2024-04-15 22:33:45 -0600

    Add home page basic content

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+  get "about" => "about#index"
+  get "contact" => "contact#index"
+  get "login" => "login#index"
 end
